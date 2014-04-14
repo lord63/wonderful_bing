93cdc9c40f6fa222a379a67a375e4c4d42cfeb26
lord63 <lord63.j@gmail.com>
2014-04-14 22:11:39 +0800

    v1.0;can set as wp;add check

--- wonderful_bing.py
+++ wonderful_bing.py
@@ -1,19 +1,23 @@
+#!/usr/bin/env python
 # -*- coding=utf-8 -*-
 
-
 import re
+import os
+import time
 import requests
 
 
-def DownloadImageFile(imgUrl):
+def DownloadAndSet(imgUrl):
     filename = imgUrl.split('/')[-1]
     local_filename = changename(filename) + ".jpg"
-    print "Download Image File=", local_filename
-    r = requests.get(imgUrl, stream=True)
-    with open("/home/lord63/pictures/"+local_filename, 'wb') as f:
+    if os.path.isfile("/home/lord63/pictures/bing/"+local_filename):
+    	return  # if the picture had been downloaded, stop doing the following things
+    r = requests.get(imgUrl, stream=True)  # to get the raw content
+    with open("/home/lord63/picture/bing/"+local_filename, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024):
             f.write(chunk)
-    return local_filename
+    print "Download Image File=", local_filename
+    setWallpaper("/home/lord63/picture/bing/"+local_filename)
 
 # make the picture name look nicer
 def changename(filename):
@@ -21,6 +25,11 @@ def changename(filename):
 	m = p.search(filename)
 	return m.group()
 
+def setWallpaper(picPath):
+    os.system('hsetroot  -extend '+ picPath)
+
+
+time.sleep(5)
 r = requests.get('http://cn.bing.com')
 
 # using regular expression to get the picture url
@@ -28,5 +37,7 @@ p = re.compile('http://s.cn.bing.net/az/hprichbg/rb/([^\']+)')
 m = p.search(r.content)
 Url = m.group()
 
-DownloadImageFile(Url)
+DownloadAndSet(Url)
+
+
 
