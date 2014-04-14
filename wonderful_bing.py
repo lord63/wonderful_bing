78068ced6832a291e92ac20df90e4e776694dae1
lord63 <lord63.j@gmail.com>
2014-04-14 22:09:23 +0800

    v0.2;optimaze names and add comment

--- wonderful_bing.py
+++ wonderful_bing.py
@@ -1,10 +1,13 @@
 # -*- coding=utf-8 -*-
+
+
 import re
 import requests
 
 
-def downloadImageFile(imgUrl):
-    local_filename = imgUrl.split('/')[-1]
+def DownloadImageFile(imgUrl):
+    filename = imgUrl.split('/')[-1]
+    local_filename = changename(filename) + ".jpg"
     print "Download Image File=", local_filename
     r = requests.get(imgUrl, stream=True)
     with open("/home/lord63/pictures/"+local_filename, 'wb') as f:
@@ -12,12 +15,18 @@ def downloadImageFile(imgUrl):
             f.write(chunk)
     return local_filename
 
+# make the picture name look nicer
+def changename(filename):
+	p = re.compile('([^_])*')
+	m = p.search(filename)
+	return m.group()
 
 r = requests.get('http://cn.bing.com')
 
-p = re.compile('http://s.cn.bing.net/az/hprichbg/rb/[^\']+')
+# using regular expression to get the picture url
+p = re.compile('http://s.cn.bing.net/az/hprichbg/rb/([^\']+)')
 m = p.search(r.content)
-
 Url = m.group()
-downloadImageFile(Url)
+
+DownloadImageFile(Url)
 
