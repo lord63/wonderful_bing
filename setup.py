0773551e1f54d9b707a9be880566b0b40d9cb31a
lord63 <lord63.j@gmail.com>
2014-07-18 09:10:17 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setuptools
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='wonderful_bing',
+    version='0.4.0',
+    description="A script download Bing's img and set as wallpaper",
+    long_description=long_description,
+    url='https://github.com/lord63/wonderful_bing',
+    author='lord63',
+    author_email='lord63.j@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'topic :: Bing :: Wallpaper',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        ],
+    keywords='bing wallpaper',
+    packages=['wonderful_bing'],
+    install_requires=['requsts'],
+    package_data={
+        'wonderful_bing': ['README.rst', 'LICENSE'],
+    },
+    entry_points={
+        'console_scripts': [
+            'wonderful_bing=wonderful_bing:main']
+    }
+    )
