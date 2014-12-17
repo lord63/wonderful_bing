d68d8266991c33799623b2049b19a3336521ba7c
lord63 <lord63.j@gmail.com>
2014-12-17 16:19:11 +0800

    Refine test

--- test.py
+++ test.py
@@ -10,26 +10,25 @@ from wonderful_bing.wonderful_bing import WonderfulBing
 
 class WonderfulBingTestCase(unittest.TestCase):
     def setUp(self):
-        self.current_dir = os.path.abspath(os.path.dirname(__file__))
-        self.current_dir_with_slash = self.current_dir + '/'
+        self.directory = os.path.abspath(os.path.dirname(__file__))
         self.not_exist_dir = '/test/'
 
     def tearDown(self):
         """delete the download picture"""
-        for root, dirs, files in os.walk(self.current_dir_with_slash):
+        for root, dirs, files in os.walk(self.directory):
             for file in files:
                 if file.endswith('.jpg'):
-                    os.remove(file)
+                    os.remove(os.path.abspath(file))
 
     def test_initial(self):
         wonderful_bing = WonderfulBing(
-            {'directory': self.current_dir_with_slash})
+            {'directory': self.directory})
         assert wonderful_bing.copyright
         assert wonderful_bing.picture_url
 
     def test_get_picture_name(self):
         wonderful_bing = WonderfulBing(
-            {'directory': self.current_dir_with_slash})
+            {'directory': self.directory})
         picture_name = wonderful_bing.get_picture_name()
         assert picture_name
 
@@ -40,17 +39,17 @@ class WonderfulBingTestCase(unittest.TestCase):
                                '-V'])
         dir_specified_status = call([
             'python', './wonderful_bing/wonderful_bing.py',
-            '-d', '{}'.format(self.current_dir_with_slash)])
+            '-d', '{}'.format(self.directory)])
         assert not version_status
         assert not help_message_status
         assert not dir_specified_status
 
     def test_picture_has_been_downloaded(self):
         call(['python', './wonderful_bing/wonderful_bing.py',
-              '-d', '{}'.format(self.current_dir_with_slash)])
+              '-d', '{}'.format(self.directory)])
         picture_has_been_downloaded_status = call([
             'python', './wonderful_bing/wonderful_bing.py',
-            '-d', '{}'.format(self.current_dir_with_slash)])
+            '-d', '{}'.format(self.directory)])
         assert not picture_has_been_downloaded_status
 
     def test_without_directory(self):
@@ -63,7 +62,6 @@ class WonderfulBingTestCase(unittest.TestCase):
             'python', './wonderful_bing/wonderful_bing.py',
             '-d', '{}'.format(self.not_exist_dir)])
         assert dir_not_exist_status
-
 
 if __name__ == '__main__':
     unittest.main()
