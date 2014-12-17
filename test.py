cc9a59bb10766612e6d99de4d0bb322f4205da67
lord63 <lord63.j@gmail.com>
2014-12-17 15:06:17 +0800

    Remove unnecessary print info

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from subprocess import call
 
 from wonderful_bing.wonderful_bing import WonderfulBing
 
+
 class WonderfulBingTestCase(unittest.TestCase):
     def setUp(self):
         self.current_dir = os.path.abspath(os.path.dirname(__file__))
@@ -21,23 +22,18 @@ class WonderfulBingTestCase(unittest.TestCase):
                     os.remove(file)
 
     def test_initial(self):
-        print "\n----------test_initial begin----------"
         wonderful_bing = WonderfulBing(
             {'directory': self.current_dir_with_slash})
         assert wonderful_bing.copyright
         assert wonderful_bing.picture_url
-        print "----------test_initial end----------"
 
     def test_get_picture_name(self):
-        print "\n----------test_get_picture_name begin----------"
         wonderful_bing = WonderfulBing(
             {'directory': self.current_dir_with_slash})
         picture_name = wonderful_bing.get_picture_name()
         assert picture_name
-        print "----------test_get_picture_name end----------"
 
     def test_common_command(self):
-        print "\n----------test_common_command begin----------"
         help_message_status = call([
             'python', './wonderful_bing/wonderful_bing.py', '-h'])
         version_status = call(['python', './wonderful_bing/wonderful_bing.py',
@@ -48,32 +44,25 @@ class WonderfulBingTestCase(unittest.TestCase):
         assert not version_status
         assert not help_message_status
         assert not dir_specified_status
-        print "----------test_common_command end----------"
 
     def test_picture_has_been_downloaded(self):
-        print "\n----------test_picture_has_been_downloaded begin----------"
         call(['python', './wonderful_bing/wonderful_bing.py',
               '-d', '{}'.format(self.current_dir_with_slash)])
         picture_has_been_downloaded_status = call([
             'python', './wonderful_bing/wonderful_bing.py',
             '-d', '{}'.format(self.current_dir_with_slash)])
         assert not picture_has_been_downloaded_status
-        print "----------test_picture_has_been_downloaded end----------"
 
     def test_without_directory(self):
-        print "\n----------test_without_directory begin----------"
         no_dir_specified_status = call([
             'python', './wonderful_bing/wonderful_bing.py'])
         assert no_dir_specified_status
-        print "----------test_without_directory end----------"
 
     def test_directory_not_exist(self):
-        print "\n----------test_directory_not_exist begin----------"
         dir_not_exist_status = call([
             'python', './wonderful_bing/wonderful_bing.py',
             '-d', '{}'.format(self.not_exist_dir)])
         assert dir_not_exist_status
-        print "----------test_directory_not_exist end----------"
 
 
 if __name__ == '__main__':
