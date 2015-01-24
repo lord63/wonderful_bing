859284c9c09fe516a7e168de37d90f5e8fd58b7c
lord63 <lord63.j@gmail.com>
2015-01-24 11:38:47 +0800

    Use a better way to detect desktop environment

--- wonderful_bing/wonderful_bing.py
+++ wonderful_bing/wonderful_bing.py
@@ -24,7 +24,6 @@ import sys
 from os import path
 import argparse
 import subprocess
-from commands import getoutput
 
 import requests
 
@@ -60,13 +59,13 @@ class WonderfulBing(object):
 
     def set_wallpaper(self, picture_path):  # pragma: no cover
         # We use this command to make it work when using cron, see #3
-        desktop_environment = self.detect_desktop_environment()
-        if desktop_environment == 'gnome':
+        desktop_environment = self.get_desktop_environment()
+        if desktop_environment in ['gnome', 'gnome2', 'cinnamon']:
             subprocess.Popen(
                 "DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set \
                 org.gnome.desktop.background picture-uri file://{0}".format(
                     picture_path), shell=True)
-        elif desktop_environment == 'xfce':
+        elif desktop_environment == 'xfce4':
             subprocess.Popen(
                 "DISPLAY=:0 xfconf-query -c xfce4-desktop -p \
                 /backdrop/screen0/monitor0/image-path -s {0}".format(
@@ -100,20 +99,71 @@ class WonderfulBing(object):
         print "Successfully set the picture as the wallpaper. :)"
         self.show_notify()
 
-    def detect_desktop_environment(self):  # pragma: no cover
-        desktop_environment = 'generic'
-        if os.environ.get('KDE_FULL_SESSION') == 'true':
-            desktop_environment = 'kde'
-        elif os.environ.get('GNOME_DESKTOP_SESSION_ID'):
-            desktop_environment = 'gnome'
-        else:
-            try:
-                info = getoutput('xprop -root')
-                if 'XFCE_DESKTOP_WINDOW' in info:
-                    desktop_environment = 'xfce'
-            except (OSError, RuntimeError):
-                pass
-        return desktop_environment
+    # I get this function from http://stackoverflow.com/a/21213358
+    def get_desktop_environment(self):  # pragma: no cover
+        # From http://stackoverflow.com/q/2035657
+        # and http://ubuntuforums.org/showthread.php?t=652320
+        # and http://ubuntuforums.org/showthread.php?t=652320
+        # and http://ubuntuforums.org/showthread.php?t=1139057
+        if sys.platform in ["win32", "cygwin"]:
+            return "windows"
+        elif sys.platform == "darwin":
+            return "mac"
+        else:  # Most likely either a POSIX system or something not much common
+            desktop_session = os.environ.get("DESKTOP_SESSION")
+            # Easier to match if we doesn't have  to deal with caracter cases
+            if desktop_session is not None:
+                desktop_session = desktop_session.lower()
+                if desktop_session in ["gnome", "unity", "cinnamon", "mate",
+                                       "xfce4", "lxde", "fluxbox", "blackbox",
+                                       "openbox", "icewm", "jwm", "afterstep",
+                                       "trinity", "kde"]:
+                    return desktop_session
+                # Special cases
+                # Canonical sets $DESKTOP_SESSION to Lubuntu rather than LXDE
+                # if using LXDE.
+                # There is no guarantee that they will not do the same with
+                # the other desktop environments.
+                elif "xfce" in (desktop_session or
+                                desktop_session.startswith("xubuntu")):
+                    return "xfce4"
+                elif desktop_session.startswith("ubuntu"):
+                    return "unity"
+                elif desktop_session.startswith("lubuntu"):
+                    return "lxde"
+                elif desktop_session.startswith("kubuntu"):
+                    return "kde"
+                elif desktop_session.startswith("razor"):  # e.g. razorkwin
+                    return "razor-qt"
+                elif desktop_session.startswith("wmaker"):  # e.g.wmaker-common
+                    return "windowmaker"
+            if os.environ.get('KDE_FULL_SESSION') == 'true':
+                return "kde"
+            elif os.environ.get('GNOME_DESKTOP_SESSION_ID'):
+                if "deprecated" not in os.environ.get(
+                   'GNOME_DESKTOP_SESSION_ID'):
+                        return "gnome2"
+            # From http://ubuntuforums.org/showthread.php?t=652320
+            elif self.is_running("xfce-mcs-manage"):
+                return "xfce4"
+            elif self.is_running("ksmserver"):
+                return "kde"
+        return "unknown"
+
+
+    def is_running(self, process):
+        # From <http://www.bloggerpolis.com/2011/05/how-to-check-if-a-
+        # process-is-running-using-python/>
+        # and <http://richarddingwall.name/2009/06/18/windows-
+        # equivalents-of-ps-and-kill-commands/>
+        try:  # Linux/Unix
+            s = subprocess.Popen(["ps", "axw"], stdout=subprocess.PIPE)
+        except:  # Windows
+            s = subprocess.Popen(["tasklist", "/v"], stdout=subprocess.PIPE)
+        for x in s.stdout:
+            if re.search(process, x):
+                return True
+        return False
 
 
 def main():
