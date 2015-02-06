9a3a96f2611c3355c332a6f12a65bd31f2c89e34
lord63 <lord63.j@gmail.com>
2015-02-06 16:09:14 +0800

    Specify your desktop environment explicitly, #18
    - make the directory optional, default is /tmp
    - use docopt to make things easier
    
    Since detect the desktop environment seems complicated, now I make
    the change that you should explicitly point this out.

--- wonderful_bing/wonderful_bing.py
+++ wonderful_bing/wonderful_bing.py
@@ -26,10 +26,11 @@ import argparse
 import subprocess
 
 import requests
+from docopt import docopt
 
 
 class WonderfulBing(object):
-    def __init__(self, config):
+    def __init__(self, arguments):
         # Get all the information we need from this url, see issue#7
         self.url = "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=\
                    1&nc=1409879295618&pid=hp"
@@ -38,7 +39,8 @@ class WonderfulBing(object):
         self.picture_url = information["url"]
         if not self.picture_url.startswith('http'):
             self.picture_url = 'http://www.bing.com' + self.picture_url
-        self.directory = path.abspath(config['directory']) + '/'
+        self.environment = arguments['ENVIRONMENT']
+        self.directory = path.abspath(arguments['--directory']) + '/'
 
     def show_notify(self):
         """show the notify to get to know the picture story"""
@@ -59,23 +61,25 @@ class WonderfulBing(object):
 
     def set_wallpaper(self, picture_path):  # pragma: no cover
         # We use this command to make it work when using cron, see #3
-        desktop_environment = self.get_desktop_environment()
-        if desktop_environment in ['gnome', 'gnome2', 'cinnamon']:
-            subprocess.Popen(
+        if self.environment in ['gnome', 'gnome2', 'cinnamon']:
+            status = subprocess.Popen(
                 "DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set \
                 org.gnome.desktop.background picture-uri file://{0}".format(
                     picture_path), shell=True)
-        elif desktop_environment == 'xfce4':
-            subprocess.Popen(
+        elif self.environment == 'xfce4':
+            status = subprocess.Popen(
                 "DISPLAY=:0 xfconf-query -c xfce4-desktop -p \
                 /backdrop/screen0/monitor0/image-path -s {0}".format(
                     picture_path), shell=True)
         else:
             sys.exit(
-                "Currently we don't support your desktop_environment: {0} \n"
+                "Currently we don't support your desktop_environment.\n"
                 "Please file an issue or make a pull request :) \n"
-                "https://github.com/lord63/wonderful_bing".format(
-                    desktop_environment))
+                "https://github.com/lord63/wonderful_bing")
+        if status.wait() == 0:
+            print "Successfully set the picture as the wallpaper. :)"
+        else:
+            print "Something bad happened, fail to set as wallpaper :("
 
     def download_and_set(self):
         picture_name = self.get_picture_name()
@@ -96,92 +100,32 @@ class WonderfulBing(object):
         print "Successfully download the picture to --> {0}.".format(
             picture_path)
         self.set_wallpaper(picture_path)
-        print "Successfully set the picture as the wallpaper. :)"
         self.show_notify()
-
-    # I get this function from http://stackoverflow.com/a/21213358
-    def get_desktop_environment(self):  # pragma: no cover
-        # From http://stackoverflow.com/q/2035657
-        # and http://ubuntuforums.org/showthread.php?t=652320
-        # and http://ubuntuforums.org/showthread.php?t=652320
-        # and http://ubuntuforums.org/showthread.php?t=1139057
-        if sys.platform in ["win32", "cygwin"]:
-            return "windows"
-        elif sys.platform == "darwin":
-            return "mac"
-        else:  # Most likely either a POSIX system or something not much common
-            desktop_session = os.environ.get("DESKTOP_SESSION")
-            # Easier to match if we doesn't have  to deal with caracter cases
-            if desktop_session is not None:
-                desktop_session = desktop_session.lower()
-                if desktop_session in ["gnome", "unity", "cinnamon", "mate",
-                                       "xfce4", "lxde", "fluxbox", "blackbox",
-                                       "openbox", "icewm", "jwm", "afterstep",
-                                       "trinity", "kde"]:
-                    return desktop_session
-                # Special cases
-                # Canonical sets $DESKTOP_SESSION to Lubuntu rather than LXDE
-                # if using LXDE.
-                # There is no guarantee that they will not do the same with
-                # the other desktop environments.
-                elif "xfce" in (desktop_session or
-                                desktop_session.startswith("xubuntu")):
-                    return "xfce4"
-                elif desktop_session.startswith("ubuntu"):
-                    return "unity"
-                elif desktop_session.startswith("lubuntu"):
-                    return "lxde"
-                elif desktop_session.startswith("kubuntu"):
-                    return "kde"
-                elif desktop_session.startswith("razor"):  # e.g. razorkwin
-                    return "razor-qt"
-                elif desktop_session.startswith("wmaker"):  # e.g.wmaker-common
-                    return "windowmaker"
-            if os.environ.get('KDE_FULL_SESSION') == 'true':
-                return "kde"
-            elif os.environ.get('GNOME_DESKTOP_SESSION_ID'):
-                if "deprecated" not in os.environ.get(
-                        'GNOME_DESKTOP_SESSION_ID'):
-                    return "gnome2"
-            # From http://ubuntuforums.org/showthread.php?t=652320
-            elif self.is_running("xfce-mcs-manage"):
-                return "xfce4"
-            elif self.is_running("ksmserver"):
-                return "kde"
-        return "unknown"
-
-    def is_running(self, process):
-        # From <http://www.bloggerpolis.com/2011/05/how-to-check-if-a-
-        # process-is-running-using-python/>
-        # and <http://richarddingwall.name/2009/06/18/windows-
-        # equivalents-of-ps-and-kill-commands/>
-        try:  # Linux/Unix
-            s = subprocess.Popen(["ps", "axw"], stdout=subprocess.PIPE)
-        except:  # Windows
-            s = subprocess.Popen(["tasklist", "/v"], stdout=subprocess.PIPE)
-        for x in s.stdout:
-            if re.search(process, x):
-                return True
-        return False
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        prog='wonderful_bing',
-        description="Wonderful_bing's configuration")
-    parser.add_argument('-V', '--version', action='version',
-                        version='%(prog)s {0}'.format(__version__))
-    parser.add_argument(
-        '-d', dest='directory',
-        help="set the directory to save Bing's imgs, end with '/'")
-    config = vars(parser.parse_args())
-    if not config['directory']:
-        sys.exit("Set the directory to save Bing's imgs first.\n"
-                 "For more information, use --help.")
-    if not path.exists(config['directory']):
+    doc = """
+A wallpaper a day, keep the doctor away.
+
+Usage:
+  bing [-d DIRECTORY] ENVIRONMENT
+  bing --version
+
+Arguments:
+  ENVIRONMENT                your desktop environment
+
+Options:
+  -h --help                  show the help info and exit
+  --version                  show the version and exit
+  -d, --directory=DIRECTORY  specify where to save the download picture
+                             [default: /tmp]
+
+"""
+    arguments = docopt(doc, version=__version__)
+    if not path.exists(arguments['--directory']):
         sys.exit('No such directory :(')
 
-    bing = WonderfulBing(config)
+    bing = WonderfulBing(arguments)
     try:
         bing.download_and_set()
     except requests.exceptions.ConnectionError:
