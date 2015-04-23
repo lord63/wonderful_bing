b8559597a5507cade602bf82e03f1020b0acd44b
lord63 <lord63.j@gmail.com>
2015-04-23 09:19:06 +0800

    Move the meta info
    * from wonderful_bing.py to __init__.py
    * delete the version.py

--- wonderful_bing/__init__.py
+++ wonderful_bing/__init__.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+"""
+    wonderful_bing
+    ~~~~~~~~~~~~~~
+
+    Wonderful_bing is a small and simple program that helps you download
+    pictures from Bing and set as wallpaper. My first python program :)
+
+    :copyright: (c) 2014 by lord63.
+    :license: MIT, see LICENSE for more details.
+"""
+
+__title__ = "wonderful_bing"
+__version__ = '0.6.2'
+__author__ = "lord63"
+__license__ = "MIT"
+__copyright__ = "Copyright 2014 lord63"
