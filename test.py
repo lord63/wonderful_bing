0beefbfd72a3996e6074f52f02745209022108fe
lord63 <lord63.j@gmail.com>
2015-04-23 09:25:06 +0800

    Improve the test

--- test.py
+++ test.py
@@ -23,46 +23,49 @@ class WonderfulBingTestCase(unittest.TestCase):
                     os.remove(self.directory + file)
 
     def test_initial(self):
-        wonderful_bing = WonderfulBing(self.arguments)
-        assert wonderful_bing.copyright
-        assert wonderful_bing.picture_url
+        bing = WonderfulBing(self.arguments)
+        self.assertIsNotNone(bing.copyright)
+        self.assertIsNotNone(bing.picture_url)
 
     def test_get_picture_name(self):
         wonderful_bing = WonderfulBing(self.arguments)
         picture_name = wonderful_bing.get_picture_name()
-        assert picture_name
+        self.assertIsNotNone(picture_name)
 
     def test_picture_has_been_downloaded(self):
-        command = "python ./wonderful_bing/wonderful_bing.py set -d \
-            {0} {1}".format(self.directory, self.arguments['ENVIRONMENT'])
+        command = "bing set -d {0} {1}".format(
+            self.directory, self.arguments['ENVIRONMENT'])
+        call(command.split())
         call(command.split())
         picture_has_been_downloaded_status = call(command.split())
-        assert not picture_has_been_downloaded_status
+        self.assertEqual(picture_has_been_downloaded_status, 0)
 
     def test_without_directory(self):
-        command = "python ./wonderful_bing/wonderful_bing.py set {0}".format(
-            self.arguments['ENVIRONMENT'])
+        command = "bing set {0}".format(self.arguments['ENVIRONMENT'])
         no_dir_specified_status = call(command.split())
-        assert not no_dir_specified_status
+        self.assertEqual(no_dir_specified_status, 0)
 
     def test_directory_not_exist(self):
-        command = "python ./wonderful_bing/wonderful_bing.py set -d \
-            {0} {1}".format(self.not_exist_dir, self.arguments['ENVIRONMENT'])
+        command = "bing -d {0} {1}".format(self.not_exist_dir,
+                                           self.arguments['ENVIRONMENT'])
         dir_not_exist_status = call(command.split())
-        assert dir_not_exist_status
+        self.assertEqual(dir_not_exist_status, 1)
 
     def test_common_command(self):
-        help_message_status = call([
-            'python', './wonderful_bing/wonderful_bing.py', '-h'])
-        version_status = call(['python', './wonderful_bing/wonderful_bing.py',
-                               '--version'])
+        help_message_status = call(['bing', '-h'])
+        version_status = call(['bing', '--version'])
         dir_specified_status = call([
-            'python', './wonderful_bing/wonderful_bing.py', 'set',
+            'bing', 'set',
             '-d', '{}'.format(self.directory),
             '{}'.format(self.arguments['ENVIRONMENT'])])
-        assert not version_status
-        assert not help_message_status
-        assert not dir_specified_status
+        self.assertEqual(version_status, 0)
+        self.assertEqual(help_message_status, 0)
+        self.assertEqual(dir_specified_status, 0)
+
+    def test_story(self):
+        story_status = call(['bing', 'story'])
+        self.assertEqual(story_status, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
